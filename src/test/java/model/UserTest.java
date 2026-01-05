88d1aabe1490e095281cdd40d2e56a9b70d75103
apple <apple@appleui-MacBookPro.local>
2026-01-05 18:31:21 +0900

    test: add initial unit test case for user creatioin

---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    @DisplayName("유저 생성 시, 입력 정보가 올바르게 저장")
+    void userCreationTest(){
+        String userId = "123";
+        String name = "lee";
+        User user = new User(userId,"pw123",name,"test@gmail.com");
+
+        assertThat(user.getUserId()).isEqualTo(userId);
+        assertThat(user.getName()).contains(name);
+        assertThat(user.getEmail()).isNotNull();
+    }
+}
