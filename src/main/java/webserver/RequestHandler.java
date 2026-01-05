9ed6f14c38508a73935ed6221df53003a40a42e4
apple <apple@appleui-MacBookPro.local>
2026-01-05 17:19:56 +0900

    refactor: extract StaticResourceProcessor from RequestHandler

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,18 +3,18 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 
-import http.MimeType;
 import http.HttpRequest;
 import http.HttpResponse;
 
-import util.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
+    private static final StaticResourceProcessor processor = new StaticResourceProcessor();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -36,7 +36,7 @@ public class RequestHandler implements Runnable {
             if (path.startsWith("/user/create")) {
                 createUser(response);
             } else {
-                responseStaticFile(path, response);
+                processor.process(path, response);
             }
         } catch (IOException e) {
             logger.error(e.getMessage());
@@ -45,40 +45,5 @@ public class RequestHandler implements Runnable {
 
     private void createUser(HttpResponse response) {
         response.response302Header("/index.html");
-
     }
-
-    private void responseStaticFile(String path, HttpResponse response) {
-        int dotIndex = path.lastIndexOf(".");
-        if (dotIndex == -1 && !path.endsWith("/")) {
-            response.response302Header(path + "/");
-            return;
-        }
-
-        if (path.endsWith("/")) {
-            path += "/index.html";
-        }
-
-        String extension = "html";
-        dotIndex = path.lastIndexOf(".");
-        if (dotIndex != -1) {
-            extension = path.substring(dotIndex + 1);
-        }
-        String contentType = MimeType.getContentType(extension);
-
-        String resourcePath = "/static" + path;
-        try (InputStream resourceStream = getClass().getResourceAsStream(resourcePath)) {
-            if (resourceStream == null) {
-                response.response404Header();
-                return;
-            }
-            byte[] body = util.IOUtils.readAllBytes(resourceStream);
-            response.response200Header(body.length, contentType);
-            response.responseBody(body);
-
-        } catch (IOException e) {
-            logger.error("file read error");
-        }
-    }
-
 }
