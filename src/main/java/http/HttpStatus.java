2c589826133f7f32124ed5324a126756fbfaef2f
apple <apple@appleui-MacBookPro.local>
2026-01-08 17:55:30 +0900

    refactor: add HttpStatus enum

---
+++ src/main/java/http/HttpStatus.java
@@ -0,0 +1,21 @@
+package http;
+
+public enum HttpStatus {
+    OK(200, "OK"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String message;
+
+    HttpStatus(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getStatusLine() {
+        return "HTTP/1.1 " + code + " " + message + " \r\n";
+    }
+}
