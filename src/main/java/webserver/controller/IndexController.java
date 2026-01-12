8f9fcb30d3b2b63d90a1a3c6176cf4ba95e287a1
apple <apple@appleui-MacBookPro.local>
2026-01-12 15:56:15 +0900

    refactor: separate HTML fragments and simpify indexController logic

--- src/main/java/webserver/controller/IndexController.java
+++ src/main/java/webserver/controller/IndexController.java
@@ -21,30 +21,9 @@ public class IndexController implements Controller {
         String sid = request.getCookie("sid");
         User user = Database.getUserBySessionId(sid);
 
-        StringBuilder authHtml = new StringBuilder();
+        String authSection = renderAuthSection(user);
+        String dynamicHtml = html.replace("{{LOGIN_SECTION}}", authSection);
 
-        if (user != null) {
-            // 1. 로그인 상태: 사용자 이름(마이페이지 연결) + 로그아웃 버튼
-            authHtml.append("<li class=\"header__menu__item\">");
-            authHtml.append("  <a class=\"btn btn_ghost btn_size_s\" href=\"/mypage\">")
-                    .append(user.getName()).append(" 님</a>");
-            authHtml.append("</li>");
-
-            authHtml.append("<li class=\"header__menu__item\">");
-            authHtml.append("  <a class=\"btn btn_contained btn_size_s\" href=\"/logout\">로그아웃</a>");
-            authHtml.append("</li>");
-        } else {
-            // 2. 비로그인 상태: 기존 HTML의 로그인/회원가입 버튼 구조 유지
-            authHtml.append("<li class=\"header__menu__item\">");
-            authHtml.append("  <a class=\"btn btn_contained btn_size_s\" href=\"/login\">로그인</a>");
-            authHtml.append("</li>");
-
-            authHtml.append("<li class=\"header__menu__item\">");
-            authHtml.append("  <a class=\"btn btn_ghost btn_size_s\" href=\"/registration\">회원 가입</a>");
-            authHtml.append("</li>");
-        }
-
-        String dynamicHtml = html.replace("{{LOGIN_SECTION}}", authHtml.toString());
         response.sendBody(dynamicHtml, ContentType.HTML); // 바뀐 내용 전달
     }
 
@@ -68,5 +47,13 @@ public class IndexController implements Controller {
         }
 
         return content;
+    }
+
+    private String renderAuthSection(User user) {
+        if (user == null) {
+            return readFile("/fragments/nav_login.html");
+        }
+        String logoutHtml = readFile("/fragments/nav_logout.html");
+        return logoutHtml.replace("{{userName}}", user.getName());
     }
 }
